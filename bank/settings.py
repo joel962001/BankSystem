e6808ce4197705144995ed90fd8d8060cb6518fc
joel962001 <joel962001@gmail.com>
2021-11-09 16:25:13 +0530

    comment staticFilesStorage

--- bank/settings.py
+++ bank/settings.py
@@ -136,6 +136,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
